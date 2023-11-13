4e6e4e3d0d9164d29e9f4cbdf417ac5c3bfa963d
Ariyo Aresa <108690633+ariyoaresa@users.noreply.github.com>
2023-11-13 02:34:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 function payWithPaystack() {
 
     var handler = PaystackPop.setup({ 
-        key: 'your_public_key', //put your public key here
-        email: 'customer@email.com', //put your customer's email here
-        amount: 370000, //amount the customer is supposed to pay
+        key: 'pk_test_3359c81f54adc93f93d7abfab133e8bba84d02c3', //put your public key here
+        email: 'ariyoaresa@gmail.com', //put your customer's email here
+        amount: 100, //amount the customer is supposed to pay
         metadata: {
             custom_fields: [
                 {
                     display_name: "Mobile Number",
                     variable_name: "mobile_number",
-                    value: "+2348012345678" //customer's mobile number
+                    value: "+2347081134216" //customer's mobile number
                 }
             ]
         },
